9e751640ed36aee127d66134d67e359f61d5d955
ThandarAungUCSM <34467468+ThandarAungUCSM@users.noreply.github.com>
2023-04-28 12:44:13 +0630

    mobile popup hight convert process pages

--- components/ConvertServiceModal.vue
+++ components/ConvertServiceModal.vue
@@ -379,7 +379,7 @@ export default {
     align-items: center;
     justify-content: space-between;
     // padding: 25px 5% 12px;
-    padding: 25px 5%;
+    padding: 17px 5%;
     background: #FFF;
     box-shadow: 10px -10px 1px rgba(0, 0, 0, 0.1);
     margin-bottom: 1px;
